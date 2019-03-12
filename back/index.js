1aabd699d2f8bce21a28b068f603851ef2b8df8f
Listar <shnap2010@yandex.ru>
2019-03-13 01:28:18 +0500

    change sendMessage  format

--- back/index.js
+++ back/index.js
@@ -1,48 +1,67 @@
-var app = require("express")();
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
-var bodyParser = require("body-parser");
-const port = 3001;
-const users = new Map();
+var app = require('express')()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var bodyParser = require('body-parser')
+const nanoID = require('nanoid')
+
+const port = 3001
+const users = new Map()
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.get("/api", function(req, res) {
-  res.send(req.body);
-  console.log(res.body);
-});
+app.get('/api', function(req, res) {
+  res.send(req.body)
+  console.log(res.body)
+})
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
 
-io.on("connection", socket => {
-  console.log("user Connection");
+io.on('connection', socket => {
+  console.log('user Connection')
 
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
 
-  socket.on("message", msg => {
-    console.log(msg);
-    io.emit("newMessage", msg);
-    // io.emit("chatMessage", { msg: msg, user: users.get(socket.id) });
-  });
+  socket.on('a', q => {
+    console.log('A socket')
+    console.log(q)
+    socket.emit('B', 'asd')
+  })
 
-  socket.on("register", name => {
-    users.set(socket.id, name);
-    console.log(name);
-  });
-});
+  socket.on('message', data => {
+    console.log(data)
+    const messageData = {
+      user: {
+        id: socket.id,
+        user: data.name,
+        src: data.src
+      },
+      message: {
+        id: nanoID(),
+        message: data.message
+      }
+    }
 
-io.on("message", msg => {
-  console.log(msg);
-});
+    io.emit('newMessage', messageData)
+  })
+
+  socket.on('register', name => {
+    users.set(socket.id, name)
+    console.log(name)
+  })
+})
+
+io.on('message', msg => {
+  console.log(msg)
+})
 
 http.listen(port, function() {
-  console.log(`listening on port *:${port}`);
-});
+  console.log(`listening on port *:${port}`)
+})
